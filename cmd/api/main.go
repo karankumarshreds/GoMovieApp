d9a8bbacbe4962a9618d70cb59b8fb103f91af49
karankumarshreds <naileditnerd@gmail.com>
2021-09-29 01:07:11 +0530

    add endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,20 @@
 package main 
 
 import (
-	"flag"
+	"fmt"
 	"log"
+	"flag"
+	"net/http"
+	"encoding/json"
 )
 
 const version = "1.0.0"
+
+type AppStatus struct {
+	Status       string 
+	Environment  string 
+	Version      string 
+}
 
 type config struct {
 	port int 
@@ -20,4 +29,21 @@ func main() {
 	flag.Parse()
 	log.Printf("Running the application on port %v in %v mode...", cfg.port, cfg.env)
 	
+
+	http.HandleFunc("/api/status", func(w http.ResponseWriter, r *http.Request){
+		currentStatus := AppStatus {
+			Status      : "Available",
+			Environment : cfg.env,
+			Version     : version,
+		}
+		err := json.NewEncoder(w).Encode(currentStatus)
+		if err !=nil {
+			log.Printf("Error sending the json response %s\n", err)
+		}
+	})
+
+	err := http.ListenAndServe(fmt.Sprintf(":%v", cfg.port), nil)
+	if err !=nil {
+		log.Fatalf("Unable to start the application")
+	}
 }

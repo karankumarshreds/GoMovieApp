b3d0ae218c6ca3cc74f2a70c83407b9104ecb613
karankumarshreds <naileditnerd@gmail.com>
2021-09-29 22:54:30 +0530

    update

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,11 +1,13 @@
 package main 
 
 import (
+	"os"
 	"fmt"
 	"log"
 	"flag"
 	"net/http"
 	"encoding/json"
+	_"github.com/gorilla/mux"
 )
 
 const version = "1.0.0"
@@ -16,39 +18,45 @@ type AppStatus struct {
 	Version      string    `json:"version"` 
 }
 
-type config struct {
-	port int 
-	env string 
+
+type Application struct {
+	config struct {
+		port int 
+		env string 
+	}
+	logger *log.Logger
 }
 
 func main() {
-	var cfg config 
+	var app Application
 
-	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
-	flag.StringVar(&cfg.env, "env", "development", "Application environment (development|production)")
+	flag.IntVar(&app.config.port, "port", 4000, "Server port to listen on")
+	flag.StringVar(&app.config.env, "env", "development", "Application environment (development|production)")
 	flag.Parse()
-	log.Printf("Running the application on port %v in %v mode...", cfg.port, cfg.env)
-	
+	log.Printf("Running the application on port %v in %v mode...", app.config.port, app.config.env)
+
+	logger := log.New(os.Stdout, "INFO \t", log.Ldate|log.Ltime)
+	app.logger = logger 
 
 	http.HandleFunc("/api/status", func(w http.ResponseWriter, r *http.Request){
 		
 		currentStatus := AppStatus {
 			Status      : "available",
-			Environment : cfg.env,
+			Environment : app.config.env,
 			Version     : version,
 		}
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		err := json.NewEncoder(w).Encode(currentStatus)
-		if err !=nil {
+		if err != nil {
 			log.Printf("Error sending the json response %s\n", err)
 		}
 
 	})
 
-	err := http.ListenAndServe(fmt.Sprintf(":%v", cfg.port), nil)
-	if err !=nil {
+	err := http.ListenAndServe(fmt.Sprintf(":%v", app.config.port), nil)
+	if err != nil {
 		log.Fatalf("Unable to start the application")
 	}
 }

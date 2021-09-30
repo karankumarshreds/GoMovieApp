8e5b4b89bc767e24b3bf9ec043f04721e648faeb
karankumarshreds <naileditnerd@gmail.com>
2021-09-30 20:30:22 +0530

    update

--- cmd/api/movie_handlers.go
+++ cmd/api/movie_handlers.go
@@ -1,9 +1,29 @@
 package main 
 
-import "net/http"
+import (
+	"time"
+	"strconv"
+	"net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/karankumarshreds/GoMovieApp/pkg/models"
+)
 
 func (app *Application) getOneMovie(w http.ResponseWriter, r *http.Request) {
-	
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		app.logger.Println("Error", err)
+	}
+	app.logger.Println("ID is", id)
+	movie := models.Movie{
+		CreatedAt: time.Now(),
+		Description: "Some random description",
+		Title: "Batman",
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(movie)
 }
 
 func (app *Application) getAllMovies(w http.ResponseWriter, r *http.Request) {
